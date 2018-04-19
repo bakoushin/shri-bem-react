4fc898a33a0d5df3a3bd9570d62b92b0a531532f
Alex Bakoushin <alex@bakoush.in>
2018-04-20 00:10:53 +0300

    Fix replace()

--- src/blocks/Article/ButtonActions/Article-ButtonActions.js
+++ src/blocks/Article/ButtonActions/Article-ButtonActions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {decl} from 'bem-react-core';
 
-import 'b:Button';
+import Button from 'b:Button';
 
 import 'm:type=large';
 import 'm:type=medium';
@@ -11,8 +11,12 @@ import 'm:type=text';
 export default decl({
   block: 'Article',
   elem: 'ButtonActions',
-  replace() {
-    return <Button mix={{block: 'Article', elem: 'ButtonActions'}}/>;
+  replace({type}) {
+    return <Button type="actions" mix={{
+      block: 'Article',
+      elem: 'ButtonActions',
+      mods: {type}
+    }}/>;
   },
   mods({type}) {
     return {...this.__base(...arguments), type};
--- src/blocks/Article/ButtonLike/Article-ButtonLike.js
+++ src/blocks/Article/ButtonLike/Article-ButtonLike.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {decl} from 'bem-react-core';
 
-import 'b:Button';
+import Button from 'b:Button';
 
 import 'm:type=large';
 import 'm:type=medium';
@@ -11,8 +11,12 @@ import 'm:type=text';
 export default decl({
   block: 'Article',
   elem: 'ButtonLike',
-  replace() {
-    return <Button mix={{block: 'Article', elem: 'ButtonLike'}}/>;
+  replace({type}) {
+    return <Button type="like" mix={{
+      block: 'Article',
+      elem: 'ButtonLike',
+      mods: {type}
+    }}/>;
   },
   mods({type}) {
     return {...this.__base(...arguments), type};
